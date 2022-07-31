6f3aa6706734bb94b8cbda119b166956161265e5
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-07-31 19:27:21 +0100

    Feat: routes configuration

---
+++ src/router.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '',
+            redirect: 'shows'
+        },
+        {
+            path: 'shows',
+            name: 'shows',
+            component: () => import('./pages/Shows.vue')
+        },
+        {
+            path: 'favorites',
+            name: 'favorites',
+            component: () => import('./pages/Favorites.vue')
+        },
+        {
+            path: '/shows/:showId',
+            name: 'show-details',
+            component: () => import('./pages/ShowDetails.vue')
+        },
+        { 
+            path: '/:notFound(.*)', 
+            component: () => import('./pages/NotFound.vue')
+        }
+    ],
+
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        }
+        return { left: 0, top };
+    },
+});
+
+export default router;
