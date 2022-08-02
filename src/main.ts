3b731c778a5db255e0240cea7a8db138750eee1b
Benedict Nkeonye <benedict@flutterwavego.com>
2022-08-02 15:14:43 +0100

    Fix: finally fixed the router madness

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/scss/main.scss'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
