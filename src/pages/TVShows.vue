1497964be18e93ec631622500850ed2e6e1b2d9d
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-08-02 17:14:08 +0100

    Feat: revamp loading component

--- src/pages/TVShows.vue
+++ src/pages/TVShows.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
-        <p>Shows here</p>
+        <Loading v-if="isLoading" />
+
+        <div class="tv-shows">
+            <p>List TV shows here</p>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import Loading from "@/components/UI/BaseLoading.vue";
+
 
 export default defineComponent({
-    name: 'TVShows'
+    name: 'TVShows',
+    components: {
+        Loading
+    },
+
+    setup() {
+        const isLoading = ref(false)
+
+        return {
+            isLoading
+        }
+    }
 })
 </script>
+
+<style lang="scss" scoped>
+    .tv-shows {
+        @include grid-flex;
+    }
+</style>
