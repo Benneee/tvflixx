b0bc5aa02a443def2755eca4ff129f2dfa4ff0d0
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-08-08 07:54:26 +0100

    WIP: episodes component

--- src/components/Episodes.vue
+++ src/components/Episodes.vue
@@ -3,9 +3,32 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import Episode from '@/types/Episode'
+import { defineComponent, onMounted, PropType } from 'vue'
 
 export default defineComponent({
     name: 'Episodes',
+
+    props: {
+        episodes: {
+            type: Array as PropType<Episode[]>,
+            required: true,
+        }
+    },
+
+    setup(props) {
+        /**
+         * ToDos Here
+         * 
+         * Create a function to sort through the episodes
+         * The function should sort episodes by their season
+         * Pass the function as an helper to the UI to create the number buttons we will have per show
+         * On clicking a button all the episodes for that season(button) should show
+         * For the display bit, use pokelogue-type of display - scatter flex
+         */
+        onMounted(() => {
+            console.log("Episodes: ", props.episodes);
+        })
+    }
 })
 </script>
