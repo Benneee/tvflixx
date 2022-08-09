3be31e3ad24252ec5480c471f1c6664a08c2fbbe
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-08-09 17:59:51 +0100

    Feat: implemented the infinite-scroll functionality

--- src/pages/TVShows.vue
+++ src/pages/TVShows.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <Loading v-if="isLoading" />
-
-        <div class="tv-shows" v-if="!isLoading && !errorOccurred">
+        <div class="tv-shows" v-if="!errorOccurred">
             <TVShowsList :tvShows="tvShows" />
+            <Observer @intersect="intersected" />
         </div>
+
 
         <div v-if="!isLoading && errorOccurred">
             <section class="tv-error">
@@ -22,24 +22,27 @@
             </section>
         </div>
     </div>
+    <Spinner v-if="isLoading" />
 </template>
 
 <script lang="ts">
 import axios from "axios";
 
 import { defineComponent, onMounted, ref } from 'vue'
-import Loading from "@/components/UI/BaseLoading.vue";
+import Spinner from "@/components/UI/BaseSpinner.vue";
 import TVShowsList from "@/components/TVShowsList.vue";
 import { apiURL } from "@/helpers";
 import Show from '@/types/Show';
 import ShowsListDataFromAPI from "@/types/ShowDataFromAPI";
+import Observer from "@/components/Observer.vue";
 
 
 export default defineComponent({
     name: 'TVShows',
     components: {
-        Loading,
+        Spinner,
         TVShowsList,
+        Observer,
     },
 
     setup() {
@@ -48,20 +51,26 @@ export default defineComponent({
         let errorOccurred = ref(false);
         let tvShows = ref<Show[]>([])
 
-        async function fetchTVShows() {
+        async function fetchTVShows(pageNumber: number = currentPage.value) {
             isLoading.value = true;
             try {
-                const response = await axios.get(`${apiURL}/most-popular`);
+                const response = await axios.get(`${apiURL}/most-popular?page=${pageNumber}`);
                 const { data } = response;
                 const { page, tv_shows }: ShowsListDataFromAPI = data
                 currentPage.value = page; // To be used later for the observer component
-                tvShows.value = tv_shows
+                let showsGotten = tv_shows
+                tvShows.value = [ ...tvShows.value || [], ...showsGotten ];
 
                 isLoading.value = false;
             } catch (error) {
                 isLoading.value = false;
                 errorOccurred.value = !!error;
             }
+        }
+
+        async function intersected() {
+            currentPage.value++
+            await fetchTVShows(currentPage.value);
         }
 
         onMounted(() => {
@@ -71,6 +80,7 @@ export default defineComponent({
         return {
             isLoading,
             currentPage,
+            intersected,
             tvShows,
             errorOccurred,
         }
